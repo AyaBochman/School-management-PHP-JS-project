c70f9e82489834fbdf1d7fcca0b3fdeca2931b9a
aya <aya.bochman@gmail.com>
2018-10-20 20:15:38 +0300

    update/add student and courses works

--- api/Controllers/courseController.php
+++ api/Controllers/courseController.php
@@ -51,53 +51,13 @@ class CourseController{
                 $targetPath = "../client/img/course.jpg";
             }
         }
-     echo json_encode($this->model->saveCourse($id,$name,$desc,$targetPath));
-        // $this->model->updateCourse($_POST['id'],$_POST['courseName'],$_POST['desc']);
+     echo json_encode($this->model->updateCourse($id,$name,$desc,$targetPath));
+        
      }
     
- 
-        //     public function del_course(){
- 
-        //         echo json_encode($this->model->deleteCourse($_POST['id']));
-            
-          
-        // }
+
 }
         
-    
-
-    // public function get_students(){
-
-    //     echo json_encode($this->model->getStudents());
-        
-    // } 
-
-    // public function get_current_student(){
-        
-    //     echo json_encode($this->model->getCurrStudent($_GET["id"]));
-    // }
-
-    // public function get_current_course(){
-        
-    //     echo json_encode($this->model->getCurrCourse($_GET["id"]));
-    // }
-
-
-    // if(isset($_GET["id"])){
-    //     echo json_encode($this->model->getStudents($_GET["id"]));
-    //     else{
-    //         echo json_encode($this->model->getStudents();
-    //     }
-    //     }
-    // public function getBenchmarks(){
-    //     $data  = $this->model->get_all_benchmarks();
-    //     echo json_encode($data);
-    // }
-
-    // public function delete(){
-    //     $data  = $this->model->delete();
-    //     echo json_encode($data);
-    // }
 
 
 
