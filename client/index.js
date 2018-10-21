4954f208531577c98c1ca64ed6041d3eb3d4ee48
aya <aya.bochman@gmail.com>
2018-10-21 22:42:30 +0300

    need to hide error on input login

--- client/index.js
+++ client/index.js
@@ -1,47 +1,8 @@
-// $( window ).on( "load", init );
-
-// function init(){
-//     console.log("the page loads");
-
-// }
 
 
 
-// function logInNow() {
+//MY LOGIN FORM VALIDATION
 
-//     var email = $("#email").val();
-//     var password = $("#password").val();
-
-//     $.ajax({
-//         method: "POST",
-//         url: "http://localhost/school/api/index.php?controller=login&action=login",
-//         data: { email: email, password: password },
-//         success: function (response) {
-//             if (response == "invalid") {
-
-//                 // return false;
-//             } else {
-//                 setTimeout(function () {
-//                     localStorage.setItem("currentUser", JSON.stringify(email));
-//                     window.location.href = response;
-//                 }, 1000)
-               
-//             }
-//             // console.log(response);
-
-
-
-//         },
-//         error: function (response) {
-//             alert("oops");
-//         }
-
-//     })
-
-// }
-
-//LOGIN
-// var working = false;
 $('.login').on('submit', function (e) {
     if ($.trim($("#email").val()) === "" || $.trim($("#password").val()) === "") {
         $('.error').html('*You need to fill all the fields');
@@ -62,21 +23,21 @@ $('.login').on('submit', function (e) {
             data: { email: email, password: password },
             success: function (response) {
                 if (response !== "http://localhost/school/client/index.html") {
+                    clearLogin();
+                    $(".error").html('*the username/password is invalid');
                     $state.html('Log in');
                     $this.removeClass('ok loading');
-                    $(".error").html('*the username/password is invalid');
+                   
+
                 } else {
                     $this.addClass('ok');
                     $state.html('Welcome back!');
                     setTimeout(function () {
                         localStorage.setItem("currentUser", JSON.stringify(email));
                         window.location.href = response;
-                    }, 1000)
+                    }, 500)
                    
                 }
-                // console.log(response);
-    
-    
     
             },
             error: function (response) {
@@ -84,31 +45,18 @@ $('.login').on('submit', function (e) {
             }
     
         })
-    
-
-
-
 
     }, 2000);
-
-
 
 
 });
 
 
-//         if (!logInNow()) {
-//           
-
-//         } else {
-//    setTimeout(function () {
-//                 $this.addClass('ok');
-//                 $state.html('Welcome back!');
-//                 logInNow();
-//             }, 500);
-            
-//         }
-
+function clearLogin(){
+    $('#email').val('');
+    $("#password").val('');
+    
+}
 
 function logout() {
     $.ajax({
@@ -128,31 +76,3 @@ function logout() {
     })
 }
 
-// $("#logout").click(function(){
-
-// })
-
-
-   // function init() {
-    //     getData();
-    // }
-
-    // init();
-
-// function getAdmins(){
-//     DOM.main.innerHTML = "";
-// }
-// function getHome() {
-//         getAll("courses");
-//         getAll("students");
-//     }
-
-
-// isLoggedIn();
-
-
-
-
-//MAIN DISPLAY
-
-// 
